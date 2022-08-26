8d2a3065133bef64f16d48ad6fcd8adc801d0029
Huda Putra Santosa <hudaputrasantosa@gmail.com>
2022-08-27 00:56:19 +0700

    up file project [GoogleController]

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use Exception;
+use App\User;
+
+class GoogleController extends Controller
+{
+    //
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    { }
+}
